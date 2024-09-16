f1a158efacf14f14372ca7abfad00e70d52f5f90
Amr-shousha <amrshousha02@gmail.com>
2024-09-16 20:38:10 +0300

    a few changes in nav

---
+++ assets/js/script.js
@@ -0,0 +1,33 @@
+// $(document).ready(function(){
+
+    // $("#try").hover(function(){
+    // $(this).hide();
+
+    // });
+
+// $("#try").dblclick(function(){
+//     $(this).hide();
+
+//     // alert("rrrr");
+// }); 
+
+// }
+// $("#try").mouseout(function(){
+//     $(this).hide();
+
+//     // alert("rrrr");
+// });    
+
+// $("#try").keypress(function(){
+//     $(this).hide();
+
+//     // alert("rrrr");
+// });    
+
+
+
+
+
+//  document.getElementById("try").onclick=func tion(){
+//     alert("clicked");
+// }
